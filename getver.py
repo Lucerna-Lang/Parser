95e8a11a86c717f60cc34a65b0a2a24bb844db01
Kyando_2 <amehikoji@gmail.com>
2021-05-27 16:41:56 -0400

    Fix meta

--- getver.py
+++ getver.py
@@ -3,6 +3,9 @@ import re
 with open("Cargo.lock", 'r') as f:
     lines = f.readlines()
 
+PARSER_VER = "UNKNOWN"
+STRUCTURES_VER = "UNKNOWN"
+LIBER_VER = "UNKNOWN"
 i = 0
 for i in range(len(lines)):
     line = lines[i]
@@ -14,4 +17,5 @@ for i in range(len(lines)):
         if "liber" in line:
             LIBER_VER = re.search(r'[\'"]([^\'"]*)[\'"]', lines[i+1]).group(1)
 
+print("Versions:")
 print("{} = v{}\n{} = v{}\n{} = v{}".format("parser", PARSER_VER, "structures", STRUCTURES_VER, "liber", LIBER_VER))
