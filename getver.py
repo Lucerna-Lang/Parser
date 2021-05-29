7a4aebdb544608b5ce7f11a3a28d162b8f2ba344
キャンド <amehikoji@gmail.com>
2021-05-29 13:56:22 -0400

    Update ver detection to detect web

--- getver.py
+++ getver.py
@@ -6,6 +6,7 @@ with open("Cargo.lock", 'r') as f:
 PARSER_VER = "UNKNOWN"
 STRUCTURES_VER = "UNKNOWN"
 LIBER_VER = "UNKNOWN"
+WEB_VER = "UNKNOWN"
 i = 0
 for i in range(len(lines)):
     line = lines[i]
@@ -16,6 +17,8 @@ for i in range(len(lines)):
             STRUCTURES_VER = re.search(r'[\'"]([^\'"]*)[\'"]', lines[i+1]).group(1)
         if "liber" in line:
             LIBER_VER = re.search(r'[\'"]([^\'"]*)[\'"]', lines[i+1]).group(1)
+        if "name = \"web\"" in line:
+            WEB_VER = re.search(r'[\'"]([^\'"]*)[\'"]', lines[i+1]).group(1)
 
 print("Versions:")
-print("{} = v{}\n{} = v{}\n{} = v{}".format("parser", PARSER_VER, "structures", STRUCTURES_VER, "liber", LIBER_VER))
+print("{} = v{}\n{} = v{}\n{} = v{}\n{} = v{}".format("parser", PARSER_VER, "structures", STRUCTURES_VER, "liber", LIBER_VER, "web", WEB_VER))
